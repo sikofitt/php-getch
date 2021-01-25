95c5b66fdc2a4c11c6b05ab35dbc6b75e1e0bcf4
R. Eric Wheeler <sikofitt@gmail.com>
2021-01-25 06:58:35 -0800

    Added strrev function to getch.c

--- src/Console/Resources/getch.c
+++ src/Console/Resources/getch.c
@@ -4,6 +4,22 @@
 #include <stdlib.h>
 static struct termios oldattr;
 
+static char *strrev(char *str)
+{
+      char *p1, *p2;
+
+      if (! str || ! *str)
+            return str;
+      for (p1 = str, p2 = str + wcslen(str) - 1; p2 > p1; ++p1, --p2)
+      {
+            *p1 ^= *p2;
+            *p2 ^= *p1;
+            *p1 ^= *p2;
+      }
+      return str;
+}
+
+
 static void setRawMode(void)
 {
     struct termios newattr;
